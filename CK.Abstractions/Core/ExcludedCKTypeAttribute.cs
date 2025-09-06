056f542b234e13ee3166abbc569514c4f232ea4c
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-09-06 14:47:53 +0200

    Renamed ExcludeCKTypeAttribute to ExcludedCKTypeAttribute.

--- CK.Abstractions/Core/ExcludedCKTypeAttribute.cs
+++ CK.Abstractions/Core/ExcludedCKTypeAttribute.cs
@@ -6,6 +6,6 @@ namespace CK.Core;
 /// Attribute that excludes a type from the Automatic DI discovering.
 /// </summary>
 [AttributeUsage( AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = false )]
-public class ExcludeCKTypeAttribute : Attribute
+public class ExcludedCKTypeAttribute : Attribute
 {
 }
