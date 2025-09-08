8745b1d45e93814140b9dabd32e2132e3f058d37
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-09-08 10:19:22 +0200

    Moved Poco attributes to Attributes/

--- CK.Abstractions/Core/Poco/IClosedPoco.cs
+++ CK.Abstractions/Core/Poco/IClosedPoco.cs
@@ -5,6 +5,10 @@ namespace CK.Core;
 /// A "Closed Poco" is a IPoco for which an interface that unifies all the IPoco interfaces of its family
 /// must be defined.
 /// <para>
+/// When a IPoco family is a IClosedPoco and the CKomposable engine cannot find an interface that unifies all
+/// the secondary IPoco definitions, this is an error.
+/// </para>
+/// <para>
 /// This introduces a constraint similar to the "unique leaf" of the <see cref="IRealObject"/>: Closed Poco
 /// can (and should) be handled through this "unique leaf". 
 /// </para>
